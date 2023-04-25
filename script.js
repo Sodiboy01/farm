bb911006dc2a54389baf9a11e298f3aa9b5585aa
Sodiboy01 <127252936+Sodiboy01@users.noreply.github.com>
2023-04-25 07:55:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('form');
+const btn = document.querySelector('button');
+const apiKey = '1fff603b28a6b4ccbaace7032bfd2d5dd49f1dc75090f5f0fbedf00e';
+ btn.addEventListener('click', e =>{
+    e.preventDefault();
+    // Check if the browser supports Geolocation API
+if (navigator.geolocation) {
+    // Get the user's current position
+    navigator.geolocation.getCurrentPosition(function(position) {
+      // Get the latitude and longitude from the position object
+      const { latitude, longitude } = position.coords;
+  
+      // Make a request to a reverse geocoding API to get the address details
+      fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`)
+        .then(response => response.json())
+        .then(data => {
+          // Extract the state and country from the address details
+          const { principalSubdivision, countryName } = data;
+  
+          // Log the state and country to the console
+          console.log(`State: ${principalSubdivision}, Country: ${countryName}`);
+        })
+        .catch(error => console.error(error));
+    });
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
+  
+ });
